e6381c89c35f55a4e09c8c8f8aa2295be11eac6a
Chowdhury-Joy <62534869+Chowdhury-Joy@users.noreply.github.com>
2021-08-02 22:50:45 +0600

    why complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
+//Hide Menu on Scroll
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header").style.top = "0";
+    } else {
+        document.getElementById("header").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
 /*==================== SHOW MENU ====================*/
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
@@ -38,55 +51,6 @@ function scrollHeader() {
     else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
-
-/*==================== Splide ====================*/
-
-
-/*==================== SWIPER DISCOVER ====================*/
-
-
-
-// var swiper = new Swiper('.swiper-container', {
-//     spaceBetween: 1,
-//     centeredSlides: true,
-//     loop: true,
-//     autoplay: {
-//         delay: 1000,
-//         disableOnInteraction: false,
-//     }
-// });
-
-/*==================== VIDEO ====================*/
-// const videoFile = document.getElementById('video-file'),
-//       videoButton = document.getElementById('video-button'),
-//       videoIcon = document.getElementById('video-icon')
-
-// function playPause(){ 
-//     if (videoFile.paused){
-//         // Play video
-//         videoFile.play()
-//         // We change the icon
-//         videoIcon.classList.add('ri-pause-line')
-//         videoIcon.classList.remove('ri-play-line')
-//     }
-//     else {
-//         // Pause video
-//         videoFile.pause(); 
-//         // We change the icon
-//         videoIcon.classList.remove('ri-pause-line')
-//         videoIcon.classList.add('ri-play-line')
-
-//     }
-// }
-// videoButton.addEventListener('click', playPause)
-
-// function finalVideo(){
-//     // Video ends, icon change
-//     videoIcon.classList.remove('ri-pause-line')
-//     videoIcon.classList.add('ri-play-line')
-// }
-// // ended, when the video ends
-// videoFile.addEventListener('ended', finalVideo)
 
 
 /*==================== SHOW SCROLL UP ====================*/
@@ -148,3 +112,5 @@ sr.reveal(`.about__img-overlay,
     origin: 'right',
     interval: 100,
 })
+
+
